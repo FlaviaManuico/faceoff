c1f0091936edff68d5468c47cedd3f23b81fcd99
FlaviaManuico <83974322+FlaviaManuico@users.noreply.github.com>
2024-06-03 13:45:58 -0500

    Update page.tsx

--- src/app/(page)/page.tsx
+++ src/app/(page)/page.tsx
@@ -4,8 +4,8 @@ import logo from "@/assets/logo-text-and-beer.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen bg-gray-100 flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen bg-gray100 flex-col items-center justify-between p-24">
       <Faceoff />
     </main>
   );
-}
+};
