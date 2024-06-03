1510f7210f8f64d83756ff6b0d1c27f0b494492e
FlaviaManuico <83974322+FlaviaManuico@users.noreply.github.com>
2024-06-03 16:01:41 -0500

    Add files via upload

---
+++ src/app/faceoffjuego/page.tsx
@@ -0,0 +1,5 @@
+import FaceoffJuego from '@/components/faceoffjuego';
+
+export default function FaceoffJuegoPage() {
+  return <FaceoffJuego />;
+}
---
+++ src/app/faceoffluces/page.tsx
@@ -0,0 +1,5 @@
+import FaceoffLuces from "@/components/faceoffluces";
+
+export default function FaceoffLucesPage() {
+  return <FaceoffLuces />;
+}
